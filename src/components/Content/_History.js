6523e8ea5367ed665fcdc858c58768186e8a22db
blayke <sr.webcode@gmail.com>
2019-07-22 17:31:40 +0800

    history controls fixed

--- src/components/Content/_History.js
+++ src/components/Content/_History.js
@@ -1,46 +1,48 @@
-import React, { Component, Fragment } from "react";
-import Datepicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+  import React, { Component, Fragment } from "react";
+  import Datepicker from "react-datepicker";
+  import "react-datepicker/dist/react-datepicker.css";
 
-export default class _History extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currDate: new Date()
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
+  export default class _History extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        currDate: new Date()
+      };
+      this.handleChange = this.handleChange.bind(this);
+    }
 
-  handleChange(val) {
-    this.setState({ currDate: val });
-  }
+    handleChange(val) {
+      this.setState({ currDate: val });
+    }
 
-  render() {
-    return (
-      <div className="history">
-        <div className="history-caption">
-          <h2>Time Machine Request</h2>
-          <p>
-            A Time Machine Request returns the observed (in the past) or
-            forecasted (in the future) hour-by-hour weather and daily weather
-            conditions for a particular date.
-          </p>
-          <Datepicker
-            className="date-picker"
-            selected={this.state.currDate}
-            onChange={this.handleChange}
-            popperPlacement="top-end"
-            showYearDropdown
-            scrollableYearDropdown
-            yearDropdownItemNumber={25}
-          />
-          <input
-            className="history-text"
-            type="text"
-            placeholder="Enter country name.."
-          />
+    render() {
+      return (
+        <div className="history">
+          <div className="history-caption">
+            <h2>Time Machine Request</h2>
+            <p>
+              A Time Machine Request returns the observed (in the past) or
+              forecasted (in the future) hour-by-hour weather and daily weather
+              conditions for a particular date.
+            </p>
+            <Datepicker
+              className="date-picker"
+              selected={this.state.currDate}
+              onChange={this.handleChange}
+              popperPlacement="top-end"
+              showYearDropdown
+              scrollableYearDropdown
+              yearDropdownItemNumber={25}
+            />
+            <input
+              className="history-text"
+              type="text"
+              placeholder="Enter country name.."
+              readOnly
+            />
+            <button className="history-btn">Get Data</button>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
-}
