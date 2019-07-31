8647c70ba4add8651228087cbeabcfcfb2b9d319
srwebcode <sr.webcode@gmail.com>
2019-07-31 12:15:55 +0800

    updated history module

--- src/components/Content/_History.js
+++ src/components/Content/_History.js
@@ -1,48 +1,76 @@
-  import React, { Component, Fragment } from "react";
-  import Datepicker from "react-datepicker";
-  import "react-datepicker/dist/react-datepicker.css";
+import React, { Component, Fragment } from "react";
+import Datepicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import Toast from "../Utils/historyToast";
 
-  export default class _History extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        currDate: new Date()
-      };
-      this.handleChange = this.handleChange.bind(this);
-    }
+//create country picker component!
 
-    handleChange(val) {
-      this.setState({ currDate: val });
-    }
-
-    render() {
-      return (
-        <div className="history">
-          <div className="history-caption">
-            <h2>Time Machine Request</h2>
-            <p>
-              A Time Machine Request returns the observed (in the past) or
-              forecasted (in the future) hour-by-hour weather and daily weather
-              conditions for a particular date.
-            </p>
-            <Datepicker
-              className="date-picker"
-              selected={this.state.currDate}
-              onChange={this.handleChange}
-              popperPlacement="top-end"
-              showYearDropdown
-              scrollableYearDropdown
-              yearDropdownItemNumber={25}
-            />
-            <input
-              className="history-text"
-              type="text"
-              placeholder="Enter country name.."
-              readOnly
-            />
-            <button className="history-btn">Get Data</button>
-          </div>
-        </div>
-      );
-    }
+export default class _History extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currDate: new Date(),
+      country: "",
+      checkerStatus: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.timeRequest = this.timeRequest.bind(this);
+    this.setCountry = this.setCountry.bind(this);
   }
+
+  setCountry(e) {
+    this.setState({ country: e.currentTarget.value });
+  }
+
+  countrySearch(val) {
+    if (val !== "test") {
+      return true;
+    }
+    return false;
+  }
+
+  timeRequest() {
+    if (this.state.country === "" || this.countrySearch(this.state.country)) {
+      return console.log(`cannot search for country name!`);
+    }
+    return console.log(`searching now.......`);
+  }
+
+  handleChange(val) {
+    this.setState({ currDate: val });
+  }
+
+  render() {
+    return (
+      <div className="history">
+        <div className="history-caption">
+          <h2>Time Machine Request</h2>
+          <p>
+            A Time Machine Request returns the observed (in the past) or
+            forecasted (in the future) hour-by-hour weather and daily weather
+            conditions for a particular date.
+          </p>
+          <Datepicker
+            className="date-picker"
+            selected={this.state.currDate}
+            onChange={this.handleChange}
+            popperPlacement="top-end"
+            showYearDropdown
+            scrollableYearDropdown
+            yearDropdownItemNumber={25}
+          />
+          <input
+            className="history-text"
+            type="text"
+            placeholder="Please enter country name..."
+            value={this.state.country}
+            onChange={this.setCountry}
+          />
+          <button className="history-btn" onClick={this.timeRequest}>
+            Get Data
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
